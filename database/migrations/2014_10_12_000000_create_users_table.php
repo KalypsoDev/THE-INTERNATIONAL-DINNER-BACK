3e83f6d80cbbb54c81d3a9b60926722f97d990b9
Angela <antunezangela89@gmail.com>
2024-03-04 16:02:57 +0100

    Modify users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,9 +13,9 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('id_role');
-            $table->string('name');
-            $table->string('number');
+            $table->string('role_id');
+            $table->string('username');
+            $table->string('phone_number');
             $table->string('email')->unique();
             $table->string('password');
             $table->rememberToken();
