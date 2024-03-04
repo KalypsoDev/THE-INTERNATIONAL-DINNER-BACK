020c1605a0b89640c1768fab94fb40d62ed1a6cb
Angela <antunezangela89@gmail.com>
2024-03-04 15:39:17 +0100

    Change Role Model

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -4,8 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Role extends Model
 {
     use HasFactory;
+    protected $guarded = []; //indicamos que campos de la tabla no se puede manipular a traves del modelo con guarded
+    public function users(): HasMany
+    {
+        return $this->hasMany(User::class, 'role_id');
+    }
 }
