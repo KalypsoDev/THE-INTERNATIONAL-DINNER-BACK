7dafeee6282a3ce52bf13480211b50a2fff4a432
Angela <antunezangela89@gmail.com>
2024-03-04 15:59:13 +0100

    Modify RoleSeeder

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Role;
 
 class RoleSeeder extends Seeder
 {
@@ -12,6 +13,7 @@ class RoleSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Role::create(['role_id' => "Buyer"]);
+        Role::create(['role_id' => "Seller"]);
     }
 }
