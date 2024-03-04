f17a51190688aea1c37cbe0c7bb47fd8245607f0
Angela <antunezangela89@gmail.com>
2024-03-04 16:13:21 +0100

    Modify UserFactory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,7 +24,8 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
+            'username' => fake()->name(),
+            'phone_number' => $this->faker->numerify('#########'),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
