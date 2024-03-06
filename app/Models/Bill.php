ed60d9d5c04b6683931ba85a4af28055a10747c5
Marta Parra <149329102+Macata47@users.noreply.github.com>
2024-03-06 16:43:42 +0100

    create models bill, diet, flag, foodallergen, product and productbill

--- app/Models/Bill.php
+++ app/Models/Bill.php
@@ -2,10 +2,30 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Bill extends Model
 {
-    use HasFactory;
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'user_id',
+        'units_quantity',
+        'total_sale_price',
+        'status',
+    ];
+
+    //definir relaciones con otras tablas si es necesario
+
+    /**
+     * Get the user that owns the bill.
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
+
--- app/Models/Diet.php
+++ app/Models/Diet.php
@@ -2,10 +2,20 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Diets extends Model
+class Diet extends Model
 {
-    use HasFactory;
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'diet_type',
+    ];
+
+    //definir relaciones con otras tablas si es necesario
+
 }
+
