73f772916cb5b040a1939d9df7130faf5710009f
Marta Parra <149329102+Macata47@users.noreply.github.com>
2024-03-06 11:02:13 +0100

    create table bills and productbills

---
+++ database/migrations/2024_03_06_094526_create_bills_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('bills', function (Blueprint $table) {
+            $table->id();
+            $table->id('id_user');
+            $table->integer('units_quantity');
+            $table->integer('total_sale_price');
+            $table->string('status')->default('pending'); 
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('bills');
+    }
+};
