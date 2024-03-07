4d014a836a5df9b8f7c68ee3eaf74f3399aa6c4d
sharonSofia <sharonsanta1626@gmail.com>
2024-03-07 16:14:39 +0300

    create Billfactories

--- database/factories/BillFactory.php
+++ database/factories/BillFactory.php
@@ -1,23 +1,18 @@
 <?php
-
 namespace Database\Factories;
-
 use Illuminate\Database\Eloquent\Factories\Factory;
-
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Bill>
- */
-class BillFactory extends Factory
-{
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
-    public function definition(): array
-    {
-        return [
-            //
-        ];
-    }
-}
+use App\Models\Bill;
+use App\Models\User; 
+use Illuminate\Support\Facades\DB;
+use Faker\Generator as Faker;
+$factory->define(Bill::class, function (Faker $faker) {
+    return [
+        'user_id' => function () {
+            
+            return User::factory()->create()->id;
+        },
+        'units_quantity' => $faker->randomNumber(2),
+        'total_sale_price' => $faker->randomFloat(2, 10, 1000),
+        'status' => $faker->randomElement(['activo', 'inactivo']),
+    ];
+});
