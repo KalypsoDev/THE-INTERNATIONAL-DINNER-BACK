73a9b30b24ce4b63939191b7c1a4d5bc9d93ada4
Marta Parra <149329102+Macata47@users.noreply.github.com>
2024-03-07 18:57:45 +0100

    seeders tables errors

--- app/Models/Bill.php
+++ app/Models/Bill.php
@@ -3,14 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Bill extends Model
 {
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
+    use HasFactory;
+    
     protected $fillable = [
         'user_id',
         'units_quantity',
--- app/Models/FoodAllergen.php
+++ app/Models/FoodAllergen.php
@@ -3,9 +3,11 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class FoodAllergen extends Model
 {
+    use HasFactory;
     
     protected $fillable = [
         'allergen_type',
